044eaf41ebc643c4b7358ea372e213b4f311fe7a
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2017-10-07 16:55:15 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
